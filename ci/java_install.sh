8ae4efbdb0017bc4cde1c08dcde4b0415c567944
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2020-08-30 19:47:17 +0200

    java command must be available in PATH environment variable
    python launches a Java process, in order to work, java command must be included in PATH environment variable

--- ci/java_install.sh
+++ ci/java_install.sh
@@ -6,3 +6,4 @@ sudo apt-get install -y openjdk-8-jdk --no-install-recommends
 sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH
